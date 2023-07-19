443e7f60bbb63d8e22a33b804c3e26f83cebbb48
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-19 22:03:37 +0200

    add ClientTest

---
+++ tests/src/Unit/ClientTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\dhl_location_finder\Unit;
+
+use Drupal\dhl_location_finder\API\Client;
+use Drupal\Tests\UnitTestCase;
+
+class ClientTest extends UnitTestCase
+{
+    public function testClientFetchesMetadata(): void
+    {
+        $client = new \GuzzleHttp\Client();
+        $client = new Client($client);
+        $data = $client->findByAddress('DE', 'Bonn', '53113');
+
+        $this->assertSame('Packstation 207', $data['locations'][0]['name']);
+        $this->assertSame('DE', $data['locations'][0]['place']['address']['countryCode']);
+    }
+}
