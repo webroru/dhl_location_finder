efe8b94e7a761302cdeadaae8340cc73579d2af9
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-21 21:44:27 +0200

    add LocationDTO

---
+++ src/API/LocationDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\dhl_location_finder\API;
+
+final readonly class LocationDTO
+{
+    public function __construct(
+        public string $url,
+        public array $location,
+        public string $name,
+        public int $distance,
+        public array $place,
+        public array $openingHours,
+        public array $closurePeriods,
+        public array $serviceTypes,
+        public array $averageCapacityDayOfWeek,
+    ) {
+    }
+}
