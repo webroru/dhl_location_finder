7227d30fd4528a17d4e1a66f2076b1666b67c059
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-21 21:09:21 +0200

    add LocationsDTOTest

--- tests/src/Unit/LocationsDTOTest.php
+++ tests/src/Unit/LocationsDTOTest.php
@@ -12,96 +12,96 @@ class LocationsDTOTest extends UnitTestCase
     public function testCreateLocationsDTO(): void
     {
         $data = [
-            "locations" => [
+            'locations' => [
                 [
-                    "url" => "/locations/8003-4008202",
-                    "location" => [
-                        "ids" => [
+                    'url' => '/locations/8003-4008202',
+                    'location' => [
+                        'ids' => [
                             [
-                                "locationId" => "8003-4008202",
-                                "provider" => "parcel"
+                                'locationId' => '8003-4008202',
+                                'provider' => 'parcel'
                             ],
                             [
-                                "locationId" => "CGN293",
-                                "provider" => "express"
+                                'locationId' => 'CGN293',
+                                'provider' => 'express'
                             ]
                         ],
-                        "keyword" => "Postfiliale",
-                        "keywordId" => "502",
-                        "type" => "postoffice"
+                        'keyword' => 'Postfiliale',
+                        'keywordId' => '502',
+                        'type' => 'postoffice'
                     ],
-                    "name" => "Postfiliale 502",
-                    "distance" => 0,
-                    "place" => [
-                        "address" => [
-                            "countryCode" => "DE",
-                            "postalCode" => "53113",
-                            "addressLocality" => "Bonn",
-                            "streetAddress" => "Charles-de-Gaulle-Str. 20"
+                    'name' => 'Postfiliale 502',
+                    'distance' => 0,
+                    'place' => [
+                        'address' => [
+                            'countryCode' => 'DE',
+                            'postalCode' => '53113',
+                            'addressLocality' => 'Bonn',
+                            'streetAddress' => 'Charles-de-Gaulle-Str. 20'
                         ],
-                        "geo" => [
-                            "latitude" => 50.71601,
-                            "longitude" => 7.129804
+                        'geo' => [
+                            'latitude' => 50.71601,
+                            'longitude' => 7.129804
                         ]
                     ],
-                    "openingHours" => [
+                    'openingHours' => [
                         [
-                            "opens" => "08:00:00",
-                            "closes" => "17:00:00",
-                            "dayOfWeek" => "http://schema.org/Monday"
+                            'opens' => '08:00:00',
+                            'closes' => '17:00:00',
+                            'dayOfWeek' => 'http://schema.org/Monday'
                         ],
                         [
-                            "opens" => "08:00:00",
-                            "closes" => "17:00:00",
-                            "dayOfWeek" => "http://schema.org/Tuesday"
+                            'opens' => '08:00:00',
+                            'closes' => '17:00:00',
+                            'dayOfWeek' => 'http://schema.org/Tuesday'
                         ],
                         [
-                            "opens" => "08:00:00",
-                            "closes" => "17:00:00",
-                            "dayOfWeek" => "http://schema.org/Wednesday"
+                            'opens' => '08:00:00',
+                            'closes' => '17:00:00',
+                            'dayOfWeek' => 'http://schema.org/Wednesday'
                         ],
                         [
-                            "opens" => "08:00:00",
-                            "closes" => "17:00:00",
-                            "dayOfWeek" => "http://schema.org/Thursday"
+                            'opens' => '08:00:00',
+                            'closes' => '17:00:00',
+                            'dayOfWeek' => 'http://schema.org/Thursday'
                         ],
                         [
-                            "opens" => "08:00:00",
-                            "closes" => "17:00:00",
-                            "dayOfWeek" => "http://schema.org/Friday"
+                            'opens' => '08:00:00',
+                            'closes' => '17:00:00',
+                            'dayOfWeek' => 'http://schema.org/Friday'
                         ],
                         [
-                            "opens" => "10:00:00",
-                            "closes" => "12:00:00",
-                            "dayOfWeek" => "http://schema.org/Saturday"
+                            'opens' => '10:00:00',
+                            'closes' => '12:00:00',
+                            'dayOfWeek' => 'http://schema.org/Saturday'
                         ]
                     ],
-                    "closurePeriods" => [
+                    'closurePeriods' => [
                     ],
-                    "serviceTypes" => [
-                        "parcel:pick-up",
-                        "parcel:drop-off-unlabeled",
-                        "postident",
-                        "express:drop-off-easy",
-                        "express:drop-off",
-                        "express:drop-off-account",
-                        "cash-on-delivery",
-                        "express:drop-off-unlabeled",
-                        "express:drop-off-prelabeled",
-                        "parcel:drop-off",
-                        "parking",
-                        "age-verification",
-                        "express:pick-up",
-                        "letter-service"
+                    'serviceTypes' => [
+                        'parcel:pick-up',
+                        'parcel:drop-off-unlabeled',
+                        'postident',
+                        'express:drop-off-easy',
+                        'express:drop-off',
+                        'express:drop-off-account',
+                        'cash-on-delivery',
+                        'express:drop-off-unlabeled',
+                        'express:drop-off-prelabeled',
+                        'parcel:drop-off',
+                        'parking',
+                        'age-verification',
+                        'express:pick-up',
+                        'letter-service'
                     ],
-                    "averageCapacityDayOfWeek" => [
+                    'averageCapacityDayOfWeek' => [
                     ]
                 ]
             ]
         ];
         $dto = new LocationsDTO(...$data);
 
-        $this->assertSame('Packstation 207', $dto->locations[0]['name']);
+        $this->assertSame('Postfiliale 502', $dto->locations[0]['name']);
         $this->assertSame('DE', $dto->locations[0]['place']['address']['countryCode']);
     }
 }
