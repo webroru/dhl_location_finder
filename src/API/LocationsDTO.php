72cc59ae813b3cb3119df2d8cf34d6e1583a2ccc
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-21 21:56:43 +0200

    add LocationDTO into LocationsDTO

--- src/API/LocationsDTO.php
+++ src/API/LocationsDTO.php
@@ -6,7 +6,14 @@ namespace Drupal\dhl_location_finder\API;
 
 final readonly class LocationsDTO
 {
-    public function __construct(public array $locations)
+    public array $locations;
+    public function __construct(array $locations)
     {
+        $collection = [];
+        foreach ($locations as $location) {
+            $collection[] = new LocationDTO(...$location);
+        }
+
+        $this->locations = $collection;
     }
 }
