dcd4a6f663122ed52b04a81dfdcf8f5f8f8d6565
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-22 17:49:01 +0200

    update LocationsDTOTest

--- tests/src/API/DTO/LocationsDTOTest.php
+++ tests/src/API/DTO/LocationsDTOTest.php
@@ -5,103 +5,40 @@ declare(strict_types=1);
 namespace Drupal\Tests\dhl_location_finder\API\DTO;
 
 use Drupal\dhl_location_finder\API\DTO\LocationsDTO;
+use Drupal\dhl_location_finder\API\Entity\Address;
+use Drupal\dhl_location_finder\API\Entity\Location;
+use Drupal\dhl_location_finder\API\Entity\OpeningHours;
+use Drupal\dhl_location_finder\API\Entity\Place;
 use Drupal\Tests\UnitTestCase;
 
 class LocationsDTOTest extends UnitTestCase
 {
     public function testCreateLocationsDTO(): void
     {
-        $data = [
-            'locations' => [
-                [
-                    'url' => '/locations/8003-4008202',
-                    'location' => [
-                        'ids' => [
-                            [
-                                'locationId' => '8003-4008202',
-                                'provider' => 'parcel'
-                            ],
-                            [
-                                'locationId' => 'CGN293',
-                                'provider' => 'express'
-                            ]
-                        ],
-                        'keyword' => 'Postfiliale',
-                        'keywordId' => '502',
-                        'type' => 'postoffice'
-                    ],
-                    'name' => 'Postfiliale 502',
-                    'distance' => 0,
-                    'place' => [
-                        'address' => [
-                            'countryCode' => 'DE',
-                            'postalCode' => '53113',
-                            'addressLocality' => 'Bonn',
-                            'streetAddress' => 'Charles-de-Gaulle-Str. 20'
-                        ],
-                        'geo' => [
-                            'latitude' => 50.71601,
-                            'longitude' => 7.129804
-                        ]
-                    ],
-                    'openingHours' => [
-                        [
-                            'opens' => '08:00:00',
-                            'closes' => '17:00:00',
-                            'dayOfWeek' => 'http://schema.org/Monday'
-                        ],
-                        [
-                            'opens' => '08:00:00',
-                            'closes' => '17:00:00',
-                            'dayOfWeek' => 'http://schema.org/Tuesday'
-                        ],
-                        [
-                            'opens' => '08:00:00',
-                            'closes' => '17:00:00',
-                            'dayOfWeek' => 'http://schema.org/Wednesday'
-                        ],
-                        [
-                            'opens' => '08:00:00',
-                            'closes' => '17:00:00',
-                            'dayOfWeek' => 'http://schema.org/Thursday'
-                        ],
-                        [
-                            'opens' => '08:00:00',
-                            'closes' => '17:00:00',
-                            'dayOfWeek' => 'http://schema.org/Friday'
-                        ],
-                        [
-                            'opens' => '10:00:00',
-                            'closes' => '12:00:00',
-                            'dayOfWeek' => 'http://schema.org/Saturday'
-                        ]
-                    ],
-                    'closurePeriods' => [
-                    ],
-                    'serviceTypes' => [
-                        'parcel:pick-up',
-                        'parcel:drop-off-unlabeled',
-                        'postident',
-                        'express:drop-off-easy',
-                        'express:drop-off',
-                        'express:drop-off-account',
-                        'cash-on-delivery',
-                        'express:drop-off-unlabeled',
-                        'express:drop-off-prelabeled',
-                        'parcel:drop-off',
-                        'parking',
-                        'age-verification',
-                        'express:pick-up',
-                        'letter-service'
-                    ],
-                    'averageCapacityDayOfWeek' => [
-                    ]
-                ]
-            ]
-        ];
-        $dto = new LocationsDTO(...$data);
+        $address = (new Address())
+            ->setCountryCode('DE')
+            ->setPostalCode('53113')
+            ->setAddressLocality('Bonn')
+            ->setStreetAddress('Charles-de-Gaulle-Str. 20')
+        ;
+        $place = (new Place())
+            ->setAddress($address)
+        ;
+
+        $openingHours = (new OpeningHours())
+            ->setOpens('08:00:00')
+            ->setCloses('17:00:00')
+            ->setDayOfWeek('http://schema.org/Monday')
+        ;
+
+        $location = (new Location())
+            ->setName('Postfiliale 502')
+            ->setPlace($place)
+            ->setOpeningHours([$openingHours])
+        ;
+        $dto = new LocationsDTO([$location]);
 
         $this->assertSame('Postfiliale 502', $dto->locations[0]->getName());
-        $this->assertSame('DE', $dto->locations[0]->getPlace->getAddress()->getCountryCode());
+        $this->assertSame('DE', $dto->locations[0]->getPlace()->getAddress()->getCountryCode());
     }
 }
