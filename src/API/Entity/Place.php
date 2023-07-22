5b9e3f1795c7ea43c5818ae9b5423815539ebd7e
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-22 15:34:49 +0200

    add Place

---
+++ src/API/Entity/Place.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\dhl_location_finder\API\Entity;
+
+class Place
+{
+    public function __construct(
+        private Address $address,
+    ) {
+    }
+
+    public function getAddress(): Address
+    {
+        return $this->address;
+    }
+    public function setAddress(Address $address): self
+    {
+        $this->address = $address;
+        return $this;
+    }
+}
