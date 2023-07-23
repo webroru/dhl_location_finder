1a6ca71b19fd2b374a10f98c3d77b07b450885da
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-23 13:47:19 +0200

    add Locations Service

---
+++ src/Service/Locations.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\dhl_location_finder\Service;
+
+use Drupal\dhl_location_finder\API\Client;
+use Drupal\dhl_location_finder\DTO\LocationsDTO;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final readonly class Locations
+{
+    public function __construct(
+        private Client $client,
+        private SerializerInterface $serializer,
+    ) {
+    }
+
+    public function findByAddress(string $countryCode, string $addressLocality, string $postalCode): LocationsDTO
+    {
+        $data = $this->client->findByAddress($countryCode, $addressLocality, $postalCode);
+        return $this->createDto($data, LocationsDTO::class);
+    }
+
+    private function createDto(string $data, string $className): object
+    {
+        return $this->serializer->deserialize($data, $className, JsonEncoder::FORMAT);
+    }
+}
