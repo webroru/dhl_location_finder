ca120dda589b09d2b40aa76aa0cb2e6c40dadc8f
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-23 13:46:52 +0200

    update LocationsTest.php

--- tests/src/Service/LocationsTest.php
+++ tests/src/Service/LocationsTest.php
@@ -5,8 +5,17 @@ declare(strict_types=1);
 namespace Drupal\Tests\dhl_location_finder\Service;
 
 use Drupal\dhl_location_finder\API\Client;
+use Drupal\dhl_location_finder\DTO\LocationsDTO;
 use Drupal\dhl_location_finder\Service\Locations;
 use Drupal\Tests\UnitTestCase;
+use Symfony\Component\PropertyInfo\Extractor\ConstructorExtractor;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class LocationsTest extends UnitTestCase
 {
@@ -14,11 +23,23 @@ class LocationsTest extends UnitTestCase
     {
         $guzzleClient = new \GuzzleHttp\Client();
         $apiClient = new Client($guzzleClient);
-        $service = new Locations($apiClient);
+        $phpDocExtractor = new PhpDocExtractor();
+        $typeExtractor   = new PropertyInfoExtractor(
+            typeExtractors: [ new ConstructorExtractor([$phpDocExtractor]), $phpDocExtractor, new ReflectionExtractor()]
+        );
 
-        $locations = $service->findByAddress('DE', 'Bonn', '53113');
+        $serializer = new Serializer(
+            normalizers: [
+                new ObjectNormalizer(propertyTypeExtractor: $typeExtractor),
+                new ArrayDenormalizer(),
+            ],
+            encoders: [new JsonEncoder()]
+        );
 
-        $this->assertSame('Postfiliale 502', $locations->locations[0]->getName());
-        $this->assertSame('DE', $locations->locations[0]->getPlace()->getAddress()->getCountryCode());
+        $service = new Locations($apiClient, $serializer);
+
+        $locationsDto = $service->findByAddress('DE', 'Bonn', '53113');
+
+        $this->assertInstanceOf(LocationsDTO::class, $locationsDto);
     }
 }
