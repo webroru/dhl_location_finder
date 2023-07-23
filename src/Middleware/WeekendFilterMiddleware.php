bcb487ed534f3567451254565e52c42e7a33c211
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-23 19:22:23 +0200

    add WeekendFilterMiddleware

---
+++ src/Middleware/WeekendFilterMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\dhl_location_finder\Middleware;
+
+use Drupal\dhl_location_finder\Entity\Location;
+use Drupal\dhl_location_finder\Entity\OpeningHours;
+
+class WeekendFilterMiddleware
+{
+    public function handle(Location $location): ?Location
+    {
+        $dasOfWeek = array_map(
+            fn(OpeningHours $openingHours) => $openingHours->getDayOfWeek(),
+            $location->getOpeningHours()
+        );
+        $isOpenOnSaturday = in_array('http://schema.org/Saturday', $dasOfWeek);
+        $isOpenOnSunday = in_array('http://schema.org/Sunday', $dasOfWeek);
+
+        return $isOpenOnSaturday && $isOpenOnSunday ? $location : null;
+    }
+}
