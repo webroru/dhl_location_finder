b779e773189025488f77b53ed4b642636b8e5d64
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-23 19:07:44 +0200

    add WeekendFilterMiddlewareTest

---
+++ tests/src/Middleware/WeekendFilterMiddlewareTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\dhl_location_finder\Middleware;
+
+use Drupal\dhl_location_finder\Entity\Location;
+use Drupal\dhl_location_finder\Entity\OpeningHours;
+use Drupal\dhl_location_finder\Middleware\WeekendFilterMiddleware;
+use Drupal\Tests\UnitTestCase;
+
+class WeekendFilterMiddlewareTest extends UnitTestCase
+{
+    public function testNullResult(): void
+    {
+        $location = (new Location())
+            ->setName('Should be filtered')
+            ->setOpeningHours([
+                (new OpeningHours())
+                    ->setDayOfWeek('http://schema.org/Monday')
+                ,
+            ])
+        ;
+
+        $middleware = new WeekendFilterMiddleware();
+
+        $result = $middleware->handle($location);
+
+        $this->assertNull($result);
+    }
+
+    public function testPassResult(): void
+    {
+        $location = (new Location())
+            ->setName('Should stay')
+            ->setOpeningHours([
+                (new OpeningHours())
+                    ->setDayOfWeek('http://schema.org/Saturday')
+                ,
+                (new OpeningHours())
+                    ->setDayOfWeek('http://schema.org/Sunday')
+                ,
+            ])
+        ;
+
+        $middleware = new WeekendFilterMiddleware();
+
+        $result = $middleware->handle($location);
+
+        $this->assertEquals('Should stay', $result->getName());
+    }
+}
