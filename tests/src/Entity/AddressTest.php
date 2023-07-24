9bc143ca90d6797224fc6ac6783ba66fb3cd2f38
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-24 20:46:50 +0200

    add AddressTest

---
+++ tests/src/Entity/AddressTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\dhl_location_finder\API\Entity;
+
+use Drupal\dhl_location_finder\Entity\Address;
+use Drupal\Tests\UnitTestCase;
+
+class AddressTest extends UnitTestCase
+{
+    public function testCreateAddressEntity(): void
+    {
+        $address = (new Address())
+            ->setCountryCode('DE')
+            ->setPostalCode('53113')
+            ->setAddressLocality('Bonn')
+            ->setStreetAddress('Charles-de-Gaulle-Str. 20')
+        ;
+
+        $this->assertEquals('DE', $address->getCountryCode());
+        $this->assertEquals('53113', $address->getPostalCode());
+        $this->assertEquals('Bonn', $address->getAddressLocality());
+        $this->assertEquals('Charles-de-Gaulle-Str. 20', $address->getStreetAddress());
+    }
+}
