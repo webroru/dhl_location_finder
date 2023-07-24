3998c22a90486cb5a0b1f011ddd6b98bbc46d4d0
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-24 20:53:40 +0200

    add OpeningHoursTest

---
+++ tests/src/Entity/OpeningHoursTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\dhl_location_finder\API\Entity;
+
+use Drupal\dhl_location_finder\Entity\OpeningHours;
+use Drupal\Tests\UnitTestCase;
+
+class OpeningHoursTest extends UnitTestCase
+{
+    public function testCreateOpeningHoursEntity(): void
+    {
+        $openingHours = (new OpeningHours())
+            ->setMonday('00:00:00 - 23:59:00')
+            ->setTuesday('00:00:00 - 23:59:00')
+            ->setWednesday('00:00:00 - 23:59:00')
+            ->setThursday('00:00:00 - 23:59:00')
+            ->setFriday('00:00:00 - 23:59:00')
+            ->setSaturday('00:00:00 - 23:59:00')
+            ->setSunday('00:00:00 - 23:59:00')
+        ;
+
+        $this->assertEquals('00:00:00 - 23:59:00', $openingHours->getMonday());
+        $this->assertEquals('00:00:00 - 23:59:00', $openingHours->getSunday());
+    }
+}
