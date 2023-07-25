9914752faf79833230f52343d636b0bdb7a9b51d
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-25 21:48:40 +0200

    use LocationAdapter in WeekendFilterMiddleware

--- src/Middleware/WeekendFilterMiddleware.php
+++ src/Middleware/WeekendFilterMiddleware.php
@@ -4,20 +4,14 @@ declare(strict_types=1);
 
 namespace Drupal\dhl_location_finder\Middleware;
 
-use Drupal\dhl_location_finder\API\Entity\Location;
-use Drupal\dhl_location_finder\API\Entity\OpeningHours;
+use Drupal\dhl_location_finder\Entity\Location;
 
 class WeekendFilterMiddleware
 {
     public function handle(Location $location): ?Location
     {
-        $dasOfWeek = array_map(
-            fn(OpeningHours $openingHours) => $openingHours->getDayOfWeek(),
-            $location->getOpeningHours()
-        );
-        $isOpenOnSaturday = in_array('http://schema.org/Saturday', $dasOfWeek);
-        $isOpenOnSunday = in_array('http://schema.org/Sunday', $dasOfWeek);
-
-        return $isOpenOnSaturday && $isOpenOnSunday ? $location : null;
+        return $location->getOpeningHours()->getSaturday() && $location->getOpeningHours()->getSunday()
+            ? $location
+            : null;
     }
 }
