91ef5f947e0652c4e7a29ab5f0f05ad6bf82873c
Igor Nemykin <i.nemykin@devcaz.com>
2023-07-26 21:21:49 +0200

    use LocationProvider in Locations service

--- src/Service/Locations.php
+++ src/Service/Locations.php
@@ -4,27 +4,18 @@ declare(strict_types=1);
 
 namespace Drupal\dhl_location_finder\Service;
 
-use Drupal\dhl_location_finder\API\Client;
 use Drupal\dhl_location_finder\API\DTO\LocationsDTO;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\SerializerInterface;
+use Drupal\dhl_location_finder\API\LocationProvider;
 
 final readonly class Locations
 {
     public function __construct(
-        private Client $client,
-        private SerializerInterface $serializer,
+        private LocationProvider $locationProvider,
     ) {
     }
 
     public function findByAddress(string $countryCode, string $addressLocality, string $postalCode): LocationsDTO
     {
-        $data = $this->client->findByAddress($countryCode, $addressLocality, $postalCode);
-        return $this->createDto($data, LocationsDTO::class);
-    }
-
-    private function createDto(string $data, string $className): object
-    {
-        return $this->serializer->deserialize($data, $className, JsonEncoder::FORMAT);
+        return $this->locationProvider->findByAddress($countryCode, $addressLocality, $postalCode);
     }
 }
